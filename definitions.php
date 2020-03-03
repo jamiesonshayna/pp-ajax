3ffb34bdc67f49ab91d3e7e0e3544700851fe22e
Shayna Jamieson <jamieson.shayna@gmail.com>
2020-03-02 16:57:26 -0800

    Finish database retreival

--- definitions.php
+++ definitions.php
@@ -13,19 +13,12 @@ try {
 
 $searchVal = $_POST['word'];
 
-$terms = array('ajax' => 'a Greek hero in the Trojan War who kills himself because the armor of Achilles is awarded to Odysseus.',
-    'button' => 'a small disk or knob sewn on to a garment, either to fasten it by being pushed through a slit made for the purpose or for decoration.',
-    'load' => 'a heavy or bulky thing that is being carried or is about to be carried.',
-    'input' => 'what is put in, taken in, or operated on by any process or system.',
-    'doctype' => 'a doctype or document type declaration is an instruction which tells the web browser about the markup language in which the current page is written.'
-);
+$sql = "SELECT definition FROM ajax WHERE word = '$searchVal'";
 
-
-$sql = "SELECT definition FROM ajax WHERE word = $searchVal";
-
-$statement = $this->_dbh->prepare($sql);
+$statement = $dbh->prepare($sql);
 
 $statement->execute();
 
-return $statement->fetch();
-//echo "<p><strong>$searchVal: </strong>{$terms[$searchVal]}</p>";
+$result = $statement->fetch();
+
+echo "<p><strong>$searchVal: </strong>{$result['definition']}</p>";
